b21821a3da111efb440f67cffc167bd72da2082b
Daniel Ferreira <danielfsjr98@gmail.com>
2022-10-12 12:44:11 -0300

    test: added usuario controller test

---
+++ spec/controllers/usuarios_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe UsuariosController, type: :controller do
+  describe "create" do
+    it "creates a user and redirect to '/'" do
+      post :create, params: {
+        usuario: {
+          email: "daniel@gmail.com",
+          nome: "daniel",
+          senha: "pass123",
+          senha_confirmation: "pass123"
+        }
+      }
+      assert_redirected_to "/"
+    end
+
+    it "return unprocessable_entity if user is invalid" do
+      post :create, params: {usuario: {email: "daniel"}}
+      assert_response(:unprocessable_entity)
+    end
+  end
+end
