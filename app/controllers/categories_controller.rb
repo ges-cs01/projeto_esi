7907cfec1186a00ccd9c52af6e58c3b9835f89f5
João Leonardo Ribeiro Abrahão <jribeiroabrahao@gmail.com>
2022-11-09 22:54:26 -0300

    added: CategoriesController.index()

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,15 @@
 # CategoriesController
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.where(user_id: session[:user_id])
+  end
+
   def new
     @category = Category.new
   end
 
   def create
-    @category = Category.new(params.require(:category).permit(:name))
+    @category = Category.new(params.require(:category).permit(:name).reverse_merge({user_id: session[:user_id]}))
     if @category.save
       redirect_to '/categories'
     else
